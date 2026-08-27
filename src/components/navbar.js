a83df7f616b97d1373146c2e7b9215271fa1ec9f
salazarxs <martinpaolo10@gmail.com>
2026-08-27 19:25:53 -0400

    feat: Add navbar component and update index.html structure

---
+++ src/components/navbar.js
@@ -0,0 +1,37 @@
+const navbar = `<nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Productos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Nosotros</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contacto</a>
+                </li>
+            </ul>
+            <div class="d-flex align-items-center" role="search">
+                
+                <button class="btn" type="submit"> <i class="bi bi-cart2"></i>carrito(0)</button>
+            </div>
+        </div>
+    </div>
+    </nav>`
+
+window.document.addEventListener('DOMContentLoaded', (e=>{
+    const nav = document.getElementById("nav")
+    if(!nav)return
+
+    nav.outerHTML = navbar
+}))
