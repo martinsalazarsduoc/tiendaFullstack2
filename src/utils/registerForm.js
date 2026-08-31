3483558892fdcbf6f0f96d1e9567f8a6547a3419
martinsalazarsduoc <mart.salazars@duocuc.cl>
2026-08-30 20:22:21 -0400

    feat: Add user registration form and user list display in admin employees page

--- src/utils/registerForm.js
+++ src/utils/registerForm.js
@@ -1,11 +1,32 @@
+
+const handleForm = (e, newUser) => {
+    e.preventDefault()
+    console.log({ newUser })
+    const user = {
+        name: newUser.name,
+        phone: newUser.phone,
+        email: newUser.email,
+        address: `${newUser.comuna}, ${newUser.region}`
+    }
+    const userList = window.localStorage.getItem("userList") ? JSON.parse(window.localStorage.getItem("userList")) : []
+    userList.push(user)
+
+    window.localStorage.setItem("userList", JSON.stringify(userList))
+    // TODO: limpiar formulario despues de guardar
+}
+
 window.document.addEventListener('DOMContentLoaded', (e) => {
     const form = document.getElementById("form-register")
 
     if (!form) return
 
-    form.addEventListener('input', (e) => {
-        const datos = Object.fromEntries(new FormData(form));
+    let newUser = undefined
 
-        console.log({ datos })
+    form.addEventListener('input', (e) => {
+        newUser = Object.fromEntries(new FormData(form));
+    })
+
+    form.addEventListener("submit", (e) => {
+        handleForm(e, newUser)
     })
 })
