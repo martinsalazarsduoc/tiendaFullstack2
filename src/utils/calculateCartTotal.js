4a3a8e59ad702017525d0e29ea68a63e0f82e42f
salazarxs <martinpaolo10@gmail.com>
2026-09-04 12:01:05 -0400

    fix: remove unnecessary console logs in cart button event handlers

--- src/utils/calculateCartTotal.js
+++ src/utils/calculateCartTotal.js
@@ -24,14 +24,12 @@ window.document.addEventListener("DOMContentLoaded", () => {
         const inputCantidad = document.getElementById(`input-${product.id}`)
 
         if (!plusButton || !minusButton || !inputCantidad) return
-        console.log("los botones existen")
         plusButton.onclick = () => {
             console.log("sumando")
             inputCantidad.value++
             return
         }
         minusButton.onclick = () => {
-            console.log('restando')
             if (inputCantidad.value <= 0) {
                 inputCantidad.value = 0
                 return
