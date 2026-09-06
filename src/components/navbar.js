9d679cf07e154cf65a4776cd5f9b4f211db53833
martinsalazarsduoc <mart.salazars@duocuc.cl>
2026-09-06 18:15:56 -0300

    feat: Enhance navbar to highlight active link based on current path

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,6 +5,7 @@
 window.document.addEventListener("DOMContentLoaded", (e) => {
     const nav = document.getElementById("nav");
     if (!nav) return;
+    const currentPath = location.pathname
     const navbar = `
     <script src="../utils/getCart.js"></script>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -18,13 +19,13 @@ window.document.addEventListener("DOMContentLoaded", (e) => {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Inicio</a>
+                    <a class="nav-link ${currentPath === "/" ? "active" : ""}" aria-current="page" href="/">Inicio</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/src/pages/products.html">Productos</a>
+                    <a class="nav-link ${currentPath === "/src/pages/products.html" ? "active" : ""}" href="/src/pages/products.html">Productos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/src/pages/news.html">Noticias</a>
+                    <a class="nav-link ${currentPath === "/src/pages/news.html" ? "active" : ""}" href="/src/pages/news.html">Noticias</a>
                 </li>
             </ul>
             <div class="d-flex align-items-center" role="search">
